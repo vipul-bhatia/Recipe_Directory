38d14b9db1f9a60e8f59715c2a1698be4ff98921
vipul bhatia <vipul0592bhatia@gmail.com>
2023-03-08 00:15:18 +0530

    .

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
+
+
+// styles
+import './Navbar.css'
+import Searchbar from './Searchbar'
+
+
+export default function Navbar() {
+  const { color , changeColor} = useTheme()
+
+  return (
+    <div className="navbar" style={{background: color}}>
+      <nav onClick={()=>changeColor('pink')}>
+        <Link to="/" className="brand">
+          <h1>Cooking Ninja</h1>
+        </Link>
+        <Searchbar />
+        <Link to="/create">Create Recipe</Link>
+      </nav>
+    </div>
+  )
+}
